3dee1c4814681ca590559b6c9e69800e17e237bd
Celso Henrique <celso.henrique@hotmail.com.br>
2020-05-16 19:48:03 -0300

    Adding dockerfile

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const io = require('socket.io')(80);
+const PORT = 8080;
+const io = require('socket.io')(PORT);
 
 io.on('connection', socket => {
   socket.on('message', msg => {
+    console.info(msg);
+
     io.emit('message', msg);
   });
 });
+
+console.info(`Starting socket.io server in port ${PORT}`);
